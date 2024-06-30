92888b821b51793b30416881917f3f1e62126caa
jayy734 <angelsoenyunt@email.com>
2024-06-30 08:12:00 +0300

    Add Dtos and Static Roles

---
+++ Core/Dtos/UpdatePermissionDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JWTAuthAspNet7WebApi.Core.Dtos
+{
+    public class UpdatePermissionDto
+    {
+
+        [Required(ErrorMessage = "UserName is required")]
+        public string UserName { get; set; }
+    }
+}
---
+++ Core/OtherObject/StaticUserRoles.cs
@@ -0,0 +1,9 @@
+﻿namespace JWTAuthAspNet7WebApi.Core.OtherObject
+{
+    public static class StaticUserRoles
+    {
+        public const string OWNER = "OWNER";
+        public const string USER = "USER";
+        public const string ADMIN = "ADMIN";
+    }
+}
