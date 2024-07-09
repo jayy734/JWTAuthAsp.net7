0d9630099fcc45197830c20690cf277e32d43586
jayy734 <angelsoenyunt@email.com>
2024-07-09 04:53:15 +0300

    customize table names

--- Core/DbContext/ApplicationDbContext.cs
+++ Core/DbContext/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using JWTAuthAspNet7WebApi.Core.Entities;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -8,5 +9,42 @@ namespace JWTAuthAspNet7WebApi.Core.DbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
 
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<IdentityUser<string>>(entity =>
+            {
+                entity.ToTable("Users");
+            });
+            builder.Entity<IdentityRole>(entity =>
+            {
+                entity.ToTable("Roles");
+            });
+            builder.Entity<IdentityUserRole<string>>(entity =>
+            {
+                entity.ToTable("UserRoles");
+
+            });
+            builder.Entity<IdentityUserClaim<string>>(entity =>
+            {
+                entity.ToTable("UserClaims");
+            });
+            builder.Entity<IdentityUserLogin<string>>(entity =>
+            {
+                entity.ToTable("UserLogins");
+            });
+            builder.Entity<IdentityUserToken<string>>(entity =>
+            {
+                entity.ToTable("UserTokens");
+            });
+            builder.Entity<IdentityRoleClaim<string>>(entity =>
+            {
+                entity.ToTable("RoleClaims");
+            });
+
+        }
     }
+
+   
 }
